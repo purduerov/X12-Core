e0e8f333a8958fad913ce4518ae3c8a2eb9ddf8d
ivanscode <ivan.kouz333@gmail.com>
2019-09-21 15:11:08 -0400

    layout update

--- surface/src/windows/Window1/Window1Layout.jsx
+++ surface/src/windows/Window1/Window1Layout.jsx
@@ -2,12 +2,36 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 import React, { Component } from 'react';
 import Titlebar from '../../components/Titlebar/Titlebar.jsx';
+import {Row, Col, Container} from 'react-bootstrap'
 
 export default class Window1Layout extends Component {
 	render() {
 		return (
 			<div>
-				<Titlebar title="Purdue ROV Primary Screen" />
+				<Titlebar title='Purdue ROV Primary Screen' />
+				<Container fluid>
+					<Row>
+						<Col>
+
+						</Col>
+						<Col xs={6}>
+							<Row xs={6}>
+
+							</Row>
+							<Row>
+								<Col>
+								
+								</Col>
+								<Col>
+								
+								</Col>
+							</Row>
+						</Col>
+						<Col>
+						
+						</Col>
+					</Row>
+				</Container>
 			</div>
 		);
 	}
