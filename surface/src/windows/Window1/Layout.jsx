feb0001c2ecc38ffdf8a8fdc7cac93c713deeec9
ivanscode <ivan.kouz333@gmail.com>
2019-09-21 14:01:49 -0400

    bootstrap setup

--- surface/src/windows/Window1/Layout.jsx
+++ surface/src/windows/Window1/Layout.jsx
@@ -1,3 +1,6 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import $ from 'jquery';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 import React, { Component } from 'react';
 
 export default class Window1Layout extends Component {
