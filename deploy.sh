ccf42abe29264e435c6fb045f636ae704dabd7b2
Jonathan Heidegger <jth3141@gmail.com>
2019-11-11 18:36:41 -0500

    deploy file update

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,7 @@
 ######################################################################
 
 
-sshpass -p "raspberry" ssh -o StrictHostKeyChecking=no pi@raspberrypi.local rm -r ros; mkdir ros;
+sshpass -p "raspberry" ssh -o StrictHostKeyChecking=no pi@raspberrypi.local rm -r ros/src; mkdir ros/src;
 sshpass -p "raspberry" scp -o StrictHostKeyChecking=no -r ros/launch pi@raspberrypi.local:ros
 sshpass -p "raspberry" scp -o StrictHostKeyChecking=no -r ros/src pi@raspberrypi.local:ros
-sshpass -p "raspberry" ssh -o StrictHostKeyChecking=no pi@raspberrypi.local rm -r ros/devel; rm -r ros/build; rm ros/src/CMakeLists.txt; catkin_make -C ros;source ros/devel/setup.bas
+sshpass -p "raspberry" scp -o StrictHostKeyChecking=no ros/piros.sh pi@raspberrypi.local:ros
