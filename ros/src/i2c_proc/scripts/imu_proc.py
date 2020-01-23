f02bef4a0e000986553d1b56d0a7761e6285ba4d
Jonathan Heidegger <jth3141@gmail.com>
2020-01-22 21:07:51 -0500

    modified IMU_proc to accomodate the axis of the IMU relative to the ROV

--- ros/src/i2c_proc/scripts/imu_proc.py
+++ ros/src/i2c_proc/scripts/imu_proc.py
@@ -4,9 +4,9 @@ import smbus
 from BNO055 import BNO055
 from shared_msgs.msg import imu_msg
 from std_msgs.msg import Bool
-PITCH_OFFSET = 0.0
-ROLL_OFFSET = 0.0
-YAW_OFFSET = 0.0
+IMU_PITCH_OFFSET = 0.0
+IMU_ROLL_OFFSET = 0.0
+IMU_YAW_OFFSET = 0.0
 class dummyIMU():
     def __init__(self):
         pass
@@ -45,17 +45,26 @@ class dummyIMU():
 
 
 
-def _reset_imu(msg):
+def _reset_imu_offsets(msg):
     global imu
-    global PITCH_OFFSET
-    global ROLL_OFFSET
-    global YAW_OFFSET
+    global IMU_PITCH_OFFSET
+    global IMU_ROLL_OFFSET
+    global IMU_YAW_OFFSET
     print "message recieved"
     if msg.data:
-        PITCH_OFFSET = imu.pitch();
-        ROLL_OFFSET = imu.roll();
-        YAW_OFFSET = imu.yaw();
+        IMU_PITCH_OFFSET = imu.pitch();
+        IMU_ROLL_OFFSET = imu.roll();
+        IMU_YAW_OFFSET = imu.yaw();
 
+#bind all angles to -180 to 180
+def clamp_angle_neg180_to_180(angle):
+    angle_0_to_360 = clamp_angle_0_to_360(angle)
+    if angle_0_to_360 > 180:
+        return angle_0_to_360 - 180 * -1.0
+    return angle_0_to_360
+#bind all angles to -180 to 180
+def clamp_angle_0_to_360(angle):
+    return (angle + 360) % 360
 
 if __name__ == "__main__":
     global imu
@@ -66,15 +75,22 @@ if __name__ == "__main__":
                           queue_size=1)
 
     sub = rospy.Subscriber('reset_imu', Bool,
-                           _reset_imu)
+                           _reset_imu_offsets)
 
     rate = rospy.Rate(50)  # 10hz
     while not rospy.is_shutdown():
         if imu.update():
             out_message = imu_msg()
             imu_data = imu.data
-            out_message.gyro = [imu.pitch() - PITCH_OFFSET, imu.yaw() - YAW_OFFSET, imu.roll() - ROLL_OFFSET]
-            out_message.accel = [imu.acceleration_x(), imu.acceleration_y(), imu.acceleration_z()]
+            #convert everything to a 0 to 360 to apply a 1d rotation then convert back to -180 to 180
+            ROV_Pitch = clamp_angle_neg180_to_180(clamp_angle_0_to_360(imu.roll()) - IMU_ROLL_OFFSET);
+            ROV_Roll =  clamp_angle_neg180_to_180(clamp_angle_0_to_360(imu.yaw()) - IMU_YAW_OFFSET);
+            ROV_Yaw = clamp_angle_neg180_to_180(clamp_angle_0_to_360(imu.pitch()) - IMU_PITCH_OFFSET);
+            out_message.gyro = [ROV_Pitch, ROV_Roll, ROV_Yaw]
+            ROV_X_Accel = imu.acceleration_z();
+            ROV_Y_Accel = imu.acceleration_x();
+            ROV_Z_Accel = imu.acceleration_y();
+            out_message.accel = [ROV_X_Accel, ROV_Y_Accel, ROV_Z_Accel]
             pub.publish(out_message)
     rate.sleep()
 
