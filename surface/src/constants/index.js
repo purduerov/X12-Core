6820c99ed1aa03b3bfeaa8ea3a125a44ee60da0b
Nicolas Fransen <me@nicolasfransen.com>
2020-01-25 13:36:25 -0500

    config changes

--- surface/src/constants/index.js
+++ surface/src/constants/index.js
@@ -1,9 +1,17 @@
 module.exports = {
+	
+	// Event seleased when any change is made to the store
+	// Listeners receive entire contents of store
 	STORE_UPDATED: 'STORE_UPDATED',
-	UPDATE_STORE: 'UPDATE_STORE',
-	CALIBRATE_CALL: 'CALIBRATE_CALL',
-	CALIBRATE_RECEIVE: 'CALIBRATE_RECEIVE',
+
+	// Event released when gamepad state is updated in the store
+	// Listeners receive gamepad state object only
 	GAMEPAD_STATE_UPDATED: 'GAMEPAD_STATE_UPDATED',
 
+	GAMEPAD_BINDING_UPDATED: 'GAMEPAD_BINDING_UPDATED',
+	GAMEPAD_SENDING_DATA: 'GAMEPAD_SENDING_DATA',
+
+	CALIBRATE_CALL: 'CALIBRATE_CALL',
+	CALIBRATE_RECEIVE: 'CALIBRATE_RECEIVE',
 	SAMPLE_UPDATE: 'SAMPLE_UPDATE'
 };
