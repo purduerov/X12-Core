a221a3ae5e015cc2d771c33f4645dd285f958660
Nicolas Fransen <me@nicolasfransen.com>
2020-02-01 14:18:43 -0500

    new camera switcher

--- surface/src/windows/Window1/Window1Layout.jsx
+++ surface/src/windows/Window1/Window1Layout.jsx
@@ -4,7 +4,7 @@ import { ipcRenderer } from 'electron';
 
 import Titlebar from '../../components/Titlebar/Titlebar.jsx';
 import ControlColumn from '../../components/ControlColumn/ControlColumn.jsx';
-import {Row, Col, Container, Button} from 'react-bootstrap';
+import { Row, Col, Container, Button } from 'react-bootstrap';
 import Camera from '../../components/Camera/Camera.jsx';
 import CameraController from '../../components/Camera/CameraController.jsx';
 import Gamepad from '../../components/Gamepad/Gamepad.jsx';
@@ -12,53 +12,45 @@ import { STORE_UPDATED, SAMPLE_UPDATE } from '../../constants';
 
 
 export default class Window1Layout extends Component {
-	constructor(props){
+	constructor(props) {
 		super(props);
 		this.state = {
-			activeCamera: 0,
 			sampleRemoteUpdate: 0
 		};
-
-		this.changeCamera = this.changeCamera.bind(this);
 	}
 
-	changeCamera(newCamera) {
-		this.setState({
-			activeCamera: newCamera
-		});
-	}
 	render() {
 		return (
-			<Container fluid style={{padding: '0'}}>
+			<Container fluid style={{ padding: '0' }}>
 				<Titlebar title='Purdue ROV Primary Screen' />
-				<Row noGutters='true' style={{height: '94%'}}>
-					<Col style={{padding: '.5rem'}}>
-						<ControlColumn>		
+				<Row noGutters='true' style={{ height: '94%' }}>
+					<Col style={{ padding: '.5rem' }}>
+						<ControlColumn>
 						</ControlColumn>
 					</Col>
 					<Col xs={6}>
-						<Row noGutters='true' style={{height: '70%', padding: '.5rem'}}>
+						<Row noGutters='true' style={{ height: '70%', padding: '.5rem' }}>
 							<Col>
 								<ControlColumn>
-									<Camera activeCamera={this.state.activeCamera}></Camera>
+									<Camera />
 								</ControlColumn>
 							</Col>
 						</Row>
-						<Row noGutters='true' style={{height: '30%'}}>
-							<Col style={{padding: '.5rem', maxWidth: '50%'}}>
+						<Row noGutters='true' style={{ height: '30%' }}>
+							<Col style={{ padding: '.5rem', maxWidth: '50%' }}>
 								<ControlColumn>
 									<Gamepad></Gamepad>
 								</ControlColumn>
 							</Col>
-							<Col style={{padding: '.5rem', maxWidth: '50%'}}>
+							<Col style={{ padding: '.5rem', maxWidth: '50%' }}>
 								<ControlColumn>
-									electron.remote example: { this.state.sampleRemoteUpdate }
+									electron.remote example: {this.state.sampleRemoteUpdate}
 								</ControlColumn>
 							</Col>
 						</Row>
 					</Col>
-					<Col style={{padding: '.5rem'}}>
-						<ControlColumn/>
+					<Col style={{ padding: '.5rem' }}>
+						<ControlColumn />
 					</Col>
 				</Row>
 			</Container>
