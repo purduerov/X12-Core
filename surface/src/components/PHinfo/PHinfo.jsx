11b82c1e8534e7a98273c0014c76f9f54c21d9f1
Ivan <ivan.kouz333@gmail.com>
2020-02-06 19:39:16 -0500

    standardized component test

--- surface/src/components/PHinfo/PHinfo.jsx
+++ surface/src/components/PHinfo/PHinfo.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import styles from './PHinfo.css';
 import {Card, Button, ListGroup} from 'react-bootstrap';
 import '../../styles/Card.css';
+import '../../styles/ListGroup.css';
 
 export default class PHinfo extends Component {
     constructor(props) {
@@ -18,12 +19,16 @@ export default class PHinfo extends Component {
 
     render() {
         return (
-            <Card className='card-custom'>
+            <Card className="card-custom">
                 <Card.Title>pH and Temperature</Card.Title>
-                <Card.Body className='card-body'>
-                    <ListGroup variant="flush" style={{backgroundColor:'#333333'}}>
-                        <ListGroup.Item style={{backgroundColor:'#333333'}}>pH: {this.props.ph}</ListGroup.Item>
-                        <ListGroup.Item style={{backgroundColor:'#333333'}}>Temperature: {this.props.temp}</ListGroup.Item>
+                <Card.Body className='card-body-custom'>
+                    <ListGroup variant="flush" className='listgroup-custom'>
+                        <ListGroup.Item className="listgroup-item-custom">
+                            pH:<span className='text-right'>{this.props.ph}</span>
+                        </ListGroup.Item>
+                        <ListGroup.Item className="listgroup-item-custom">
+                            Temperature:<span className='text-right'>{this.props.temp}</span>
+                        </ListGroup.Item>
                     </ListGroup>
                 </Card.Body>
                 <Button variant='primary' onClick={this.freeze}>Freeze/Unfreeze Display</Button>     
