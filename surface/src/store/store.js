e6d67f371f2b030a608a3f777e016a1e6eea8b23
Nicolas Fransen <me@nicolasfransen.com>
2020-02-11 20:15:15 -0500

    added lodash merge to update store data

--- surface/src/store/store.js
+++ surface/src/store/store.js
@@ -1,9 +1,9 @@
 const EventEmitter = require('events');
+const _ = require('lodash');
 
 const {
 	STORE_UPDATED,
 	GAMEPAD_STATE_UPDATED,
-	GAMEPAD_BINDING_UPDATED,
 	IMU_UPDATED
 } = require('../constants');
 const defaults = require('./store_defaults.json');
@@ -24,10 +24,6 @@ class Store extends EventEmitter {
 			case GAMEPAD_STATE_UPDATED:
 				this.emit(GAMEPAD_STATE_UPDATED, this.data.gamepad.state);
 				break;
-			
-			case GAMEPAD_BINDING_UPDATED:
-				this.emit(GAMEPAD_BINDING_UPDATED, this.data.gamepad.binding);
-				break;
 
 			case IMU_UPDATED:
 				this.emit(IMU_UPDATED, this.data.sensors.imu);
@@ -39,22 +35,15 @@ class Store extends EventEmitter {
 	}
 
 	updateImuState(newImustate) {
-		this.data.sensors.imu = newImustate;
+		_.merge(this.data.sensors.imu, newImustate);
 
 		this.fireUpdateEvents(IMU_UPDATED);
-
 	}
 
 	updateGamepadState(newGamepadState) {
-		this.data.gamepad.state = newGamepadState;
+		_.merge(this.data.gamepad.state, newGamepadState);
 
 		this.fireUpdateEvents(GAMEPAD_STATE_UPDATED);
-	}
-
-	updateGamepadBinding(newBinding) {
-		this.data.gamepad.binding = newBinding;
-
-		this.fireUpdateEvents(GAMEPAD_BINDING_UPDATED);
 	}
 }
 
