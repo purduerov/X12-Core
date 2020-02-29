691c2a0871d9cb3dbce293a06addd2dc6c1ca96a
jasonbleasdell <jason_bleasdell@hotmail.com>
2020-02-29 04:29:23 +0000

    fixes file structure

--- ros/src/i2c_proc/src/scripts/depth_proc.py
+++ ros/src/i2c_proc/src/scripts/depth_proc.py
@@ -8,12 +8,12 @@ if __name__ == "__main__":
     rospy.init_node('depth_proc')
 
     depth_sensor = None
-    try:
-        depth_sensor = ms5837.MS5837(1)  # Initialize sensor on i2c bus 1
-        depth_sensor.init()  # Initializes with density of freshwater
-    except:
+    # try:
+    depth_sensor = MS5837(1)  # Initialize sensor on i2c bus 1
+    depth_sensor.init()  # Initializes with density of freshwater
+   # except:
 
-        pass
+    #    pass
 
     pub = rospy.Publisher('depth',
                           Float32, queue_size=10);
@@ -21,9 +21,9 @@ if __name__ == "__main__":
     rate = rospy.Rate(10)  # 10hz
     # TODO: I2C related activities
     while not rospy.is_shutdown():
-        try:
-            depth = sensor.depth()
-        except:
-            depth = 0
-        pub.publish(Float32(depth))
-        rate.sleep()
+       # try:
+       	 depth = depth_sensor.depth()
+       # except:
+       #     depth = 0
+         pub.publish(Float32(depth))
+         rate.sleep()
