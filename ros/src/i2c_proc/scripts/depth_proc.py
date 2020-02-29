aa59233188ed1283e1960043a6cbc7ff334306c6
jasonbleasdell <jason_bleasdell@hotmail.com>
2020-02-29 18:50:11 +0000

    depth_proc works now

--- ros/src/i2c_proc/scripts/depth_proc.py
+++ ros/src/i2c_proc/scripts/depth_proc.py
@@ -8,32 +8,24 @@ if __name__ == "__main__":
     rospy.init_node('depth_proc')
 
     depth_sensor = None
-    # try:
-    depth_sensor = ms5837.MS5837(1)  # Initialize sensor on i2c bus 1
-    depth_sensor.init()  # Initializes with density of freshwater
-   # except:
+    try:
+    	depth_sensor = ms5837.MS5837(1)  # Initialize sensor on i2c bus 1
+    	depth_sensor.init()  # Initializes with density of freshwater
+    except:
 
-    #    pass
-
+        pass
+    
     pub = rospy.Publisher('depth',
                           Float32, queue_size=10);
 
     rate = rospy.Rate(10)  # 10hz
     # TODO: I2C related activities
     while not rospy.is_shutdown():
-       # try:
-       	 depth = depth_sensor.depth()
-	 depth_sensor.read() #NEED THIS STATEMENT to actually update the values
-#	 if depth_sensor.read():
-	 print("P: %0.1f mbar  %0.3f psi\tT: %0.2f C  %0.2f F") % (
-         depth_sensor.pressure(), # Default is mbar (no arguments)
-	 depth_sensor.pressure(ms5837.UNITS_psi), # Request psi
-         depth_sensor.temperature(), # Default is degrees C (no arguments)
-         depth_sensor.temperature(ms5837.UNITS_Farenheit)) # Request Farenheit
-       
- #        else:
-#	       	 print("Sensor read failed :(")
-	# except:
-       #     depth = 0
-         pub.publish(Float32(depth))
-         rate.sleep()
+       try:	
+	   depth_sensor.read() #allows the sensor to read new data
+           depth = depth_sensor.depth()
+	 
+       except:
+           depth = 0
+       pub.publish(Float32(depth))
+       rate.sleep()
